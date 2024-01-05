88443eb62dc65aea87b1918583f779a08e67300d
irfanplazuardi <irfanplazuardi@gmail.com>
2024-01-05 23:39:20 +0800

    update react login register category

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Navigate, Outlet, Route, Routes, useNavigate } from "react-router-dom";
+import "./App.css";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import List from "./pages/category/List";
+import Edit from "./pages/category/Edit";
+import Add from "./pages/category/Add";
+import { Provider } from "./Provider";
+import PublicLayout from "./layouts/PublicLayout";
+import { useMemo } from "react";
+
+const PrivateOutlet = () => {
+  const token = window.localStorage.getItem("token");
+  const navigate = useNavigate();
+  const isAuth = useMemo(() => !!token, [navigate]);
+  return isAuth ? <Outlet /> : <Navigate to="/login" />;
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider>
+      <Routes>
+        <Route element={<PublicLayout />}>
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route element={<PrivateOutlet />}>
+            <Route path="/" element={<List />} />
+            <Route path="/add" element={<Add />} />
+            <Route path="/edit/:id" element={<Edit />} />
+          </Route>
+        </Route>
+      </Routes>
+    </Provider>
   );
 }
 
---
+++ src/pages/auth/Login.tsx
@@ -0,0 +1,102 @@
+import { Button, Card, CardContent, TextField, Typography } from "@mui/material"
+import { useNavigate } from "react-router-dom";
+import { Controller, useForm } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
+import axios, { AxiosError } from "axios";
+import { AppContext, ContextType } from "../../Provider";
+import { useContext } from "react";
+
+interface FormProps {
+    email?: string;
+    password?: string
+}
+
+const schema = yup
+  .object({
+    email: yup.string().email().required(),
+    password: yup.string().min(5).required(),
+  })
+.required()
+
+const Login = () => {
+    const context = useContext<ContextType>(AppContext)
+    const setOpen = context?.setOpen
+    const setMessage = context?.setMessage
+
+    const {
+        handleSubmit,
+        control,
+        formState: { errors },
+    } = useForm({
+        resolver: yupResolver(schema),
+        defaultValues: {
+            email: undefined,
+            password: undefined
+        }
+    })
+
+    const handleError = (message: string) => {
+        setOpen?.(true)
+        setMessage?.(message)
+    }
+
+    const onSubmit = async (data: FormProps) => {
+        try {
+            const response = await axios.post('https://mock-api.arikmpt.com/api/user/login', {
+                email: data.email,
+                password: data.password
+            })
+
+            window.localStorage.setItem('token', response.data.data.token)
+            navigate('/')
+        } catch (error) {
+            const err = error as AxiosError as any
+            const errors = err.response?.data?.errors
+            if(Array.isArray(errors)) {
+                return
+            }
+            handleError(errors)
+        }
+        
+    }
+
+    const navigate = useNavigate();
+
+    const handleRegister = () => {
+        navigate('/register')
+    }
+    return (
+        <div className="login-box">
+            <Card sx={{ maxWidth: 300 }}>
+                <CardContent className={'login-content'}>
+                    <Typography sx={{ fontSize: 14 }}>
+                        Please Login To Continue
+                    </Typography>
+                    <div className="login-form">
+                        <Controller
+                            name="email"
+                            control={control}
+                            render={({ field }) => <TextField value={field.value} onChange={field.onChange} label="email" variant="outlined" size="small" 
+                                helperText={errors.email?.message} error={!!errors.email}/>}
+                        />
+                        
+                        <Controller
+                            name="password"
+                            control={control}
+                            render={({ field }) => <TextField type="password" value={field.value} onChange={field.onChange} label="password" variant="outlined" size="small" 
+                                helperText={errors.password?.message} error={!!errors.password}/>}
+                        />
+                    </div>
+                    <Button variant="contained" fullWidth onClick={handleSubmit(onSubmit)}>Login</Button>
+                    <Typography sx={{ fontSize: 14 }}>
+                        Or
+                    </Typography>
+                    <Button variant="outlined" fullWidth onClick={handleRegister}>Register</Button>
+                </CardContent>
+            </Card>
+        </div>
+    )
+}
+
+export default Login
